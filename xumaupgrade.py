d3bd41c1c6f18a42b628652903036ff438e593aa
wolfcryptogroup <38184096+wolfcryptogroup@users.noreply.github.com>
2018-04-15 16:27:31 +1000

    Update xumaupgrade.py

--- xumaupgrade.py
+++ xumaupgrade.py
@@ -113,6 +113,7 @@ os.system('systemctl enable xuma')
 os.system('systemctl start xuma')
 os.system('systemctl --no-pager status xuma')
 
-print(YELLOW+"Xuma Masternode Upgrade Finished! Your Xuma Masternode is Version 1.1.0.")
+print(YELLOW+"Xuma Masternode Upgrade Finished! Your masternode version will bi displayed below.  If it displays anything other than 'Xuma Core RPC client version 1.1.0
+' something has gone wrong!.")
 time.sleep(3)
 os.system('su - xuma -c "xuma-cli | grep 1.1.0" ')
